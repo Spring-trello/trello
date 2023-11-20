d15078db870d9dc5ac739f88d6dab1cf12974123
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-20 21:47:50 +0900

    테스트 코드가 mysql db와 연결이 되어있지 않아서 통과를 못하는 관계로 jenkins가 있는 ec2 환경에서 build가 되지 않는 오류 발생하여, application-test.yml 를 작성하여 test 환경에서는 내장h2로 할 수 있도록 함

--- src/test/java/com/example/hanghaero/HanghaeroApplicationTests.java
+++ src/test/java/com/example/hanghaero/HanghaeroApplicationTests.java
@@ -2,12 +2,13 @@ package com.example.hanghaero;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HanghaeroApplicationTests {
-
 	@Test
-	void contextLoads() {
+	public void test() {
+		assert true;
 	}
-
 }
