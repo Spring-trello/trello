5f826856f7beb18af3ef57e47587708877b5818a
jungmin.park <pjm9673@gmail.com>
2023-11-22 15:13:02 +0900

    colum api

--- src/main/java/com/example/hanghaero/entity/Col.java
+++ src/main/java/com/example/hanghaero/entity/Col.java
@@ -1,7 +1,50 @@
 package com.example.hanghaero.entity;
 
+import com.example.hanghaero.dto.column.ColRequestDto;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name="columns")
 public class Col {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long columnId;
+
+	@Column(name="title")
+	String title;
+
+	@Column(name="position")
+	int position = 0;
+
+	@ManyToOne
+	@JoinColumn(name="board_id")
+	private Board board;
+
+	public Col(Board board, ColRequestDto requestDto, int lastPosition) {
+		this.title = requestDto.getTitle();
+		this.position = lastPosition+1;
+		this.board = board;
+	}
+
+	public void update(ColRequestDto requestDto) {
+		this.title = requestDto.getTitle();
+	}
+
+	public void updatePosition(int newPosition) {
+		this.position = newPosition;
+	}
 }
