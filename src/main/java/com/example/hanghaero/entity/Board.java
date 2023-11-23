a0f497b8c0f58b994ced6d6d5ad4530b021aa232
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-23 16:09:43 +0900

    board entity가 user entity와 OneToOne 관계였던 부분 ManyToOne으로 수정

--- src/main/java/com/example/hanghaero/entity/Board.java
+++ src/main/java/com/example/hanghaero/entity/Board.java
@@ -10,8 +10,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -37,12 +38,13 @@ public class Board {
 	@Column(nullable = false)
 	private String description;
 
-	@OneToOne
+	@ManyToOne
+	@JoinColumn(name = "creator_id")
 	private User user;
 
 	@OneToMany(mappedBy = "board", orphanRemoval = true)
 	private List<BoardUser> boardUserList = new ArrayList<>();
-	
+
 	public Board(BoardRequestDto boardRequestDto) {
 		this.name = boardRequestDto.getName();
 		this.bgColor = boardRequestDto.getBgColor();
