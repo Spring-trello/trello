a35c8abd55d7b660505ec97a72d2f2fe9691e1b6
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-23 16:10:17 +0900

    쉬운 테스트를 위해 Builder 패턴 적용

--- src/main/java/com/example/hanghaero/dto/CommentRequestDto.java
+++ src/main/java/com/example/hanghaero/dto/CommentRequestDto.java
@@ -1,9 +1,11 @@
 package com.example.hanghaero.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class CommentRequestDto {
 	private String contents;
--- src/main/java/com/example/hanghaero/dto/board/BoardRequestDto.java
+++ src/main/java/com/example/hanghaero/dto/board/BoardRequestDto.java
@@ -1,8 +1,12 @@
 package com.example.hanghaero.dto.board;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
+@AllArgsConstructor
 public class BoardRequestDto {
 
 	private String name;
