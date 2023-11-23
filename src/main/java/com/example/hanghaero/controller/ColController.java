6088e87cd3d5bed601f5a4e38b456fc7a333cc17
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-23 23:00:41 +0900

    다른 Controller 와의 통일성을 위해 board -> boards 로변경

--- src/main/java/com/example/hanghaero/controller/ColController.java
+++ src/main/java/com/example/hanghaero/controller/ColController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.hanghaero.dto.column.ColRequestDto;
@@ -17,26 +18,29 @@ import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/boards/{boardId}/columns")
 public class ColController {
 	private final ColService columnService;
 
-	@PostMapping("/board/{boardId}/columns")
-	public ResponseEntity createColumn(@PathVariable Long boardId, @RequestBody ColRequestDto columnRequestDto){
+	@PostMapping("")
+	public ResponseEntity createColumn(@PathVariable Long boardId, @RequestBody ColRequestDto columnRequestDto) {
 		return new ResponseEntity(columnService.createColumn(boardId, columnRequestDto), HttpStatus.OK);
 	}
 
-	@PutMapping("/board/{boardId}/columns/update/{columnId}")
-	public ResponseEntity updateColumn(@PathVariable Long boardId, @PathVariable Long columnId, @RequestBody ColRequestDto columnRequestDto){
+	@PutMapping("/update/{columnId}")
+	public ResponseEntity updateColumn(@PathVariable Long boardId, @PathVariable Long columnId,
+		@RequestBody ColRequestDto columnRequestDto) {
 		return new ResponseEntity(columnService.updateColumn(boardId, columnId, columnRequestDto), HttpStatus.OK);
 	}
 
-	@DeleteMapping("/board/{boardId}/columns/drop/{columnId}")
-	public ResponseEntity deleteColumn(@PathVariable Long boardId, @PathVariable Long columnId){
+	@DeleteMapping("/drop/{columnId}")
+	public ResponseEntity deleteColumn(@PathVariable Long boardId, @PathVariable Long columnId) {
 		return new ResponseEntity(columnService.deleteColumn(boardId, columnId), HttpStatus.OK);
 	}
 
-	@PutMapping("/borad/{boardId}/columns/move/{columnId}")
-	public ResponseEntity updateColumn(@PathVariable Long boardId, @PathVariable Long columnId, @RequestParam("position") int newPosition){
+	@PutMapping("/move/{columnId}")
+	public ResponseEntity updateColumn(@PathVariable Long boardId, @PathVariable Long columnId,
+		@RequestParam("position") int newPosition) {
 		return new ResponseEntity(columnService.moveColumn(boardId, columnId, newPosition), HttpStatus.OK);
 	}
 }
