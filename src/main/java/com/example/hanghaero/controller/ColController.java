f87f45936849ed91b0d2564c993e9bed4d6b9879
kangminbeom <alsqja6945@naver.com>
2023-11-25 13:20:10 +0900

    feat: colController moveColumn url /borad-> /board 수정

--- src/main/java/com/example/hanghaero/controller/ColController.java
+++ src/main/java/com/example/hanghaero/controller/ColController.java
@@ -21,22 +21,24 @@ public class ColController {
 	private final ColService columnService;
 
 	@PostMapping("/board/{boardId}/columns")
-	public ResponseEntity createColumn(@PathVariable Long boardId, @RequestBody ColRequestDto columnRequestDto){
+	public ResponseEntity createColumn(@PathVariable Long boardId, @RequestBody ColRequestDto columnRequestDto) {
 		return new ResponseEntity(columnService.createColumn(boardId, columnRequestDto), HttpStatus.OK);
 	}
 
 	@PutMapping("/board/{boardId}/columns/update/{columnId}")
-	public ResponseEntity updateColumn(@PathVariable Long boardId, @PathVariable Long columnId, @RequestBody ColRequestDto columnRequestDto){
+	public ResponseEntity updateColumn(@PathVariable Long boardId, @PathVariable Long columnId,
+		@RequestBody ColRequestDto columnRequestDto) {
 		return new ResponseEntity(columnService.updateColumn(boardId, columnId, columnRequestDto), HttpStatus.OK);
 	}
 
 	@DeleteMapping("/board/{boardId}/columns/drop/{columnId}")
-	public ResponseEntity deleteColumn(@PathVariable Long boardId, @PathVariable Long columnId){
+	public ResponseEntity deleteColumn(@PathVariable Long boardId, @PathVariable Long columnId) {
 		return new ResponseEntity(columnService.deleteColumn(boardId, columnId), HttpStatus.OK);
 	}
 
-	@PutMapping("/borad/{boardId}/columns/move/{columnId}")
-	public ResponseEntity updateColumn(@PathVariable Long boardId, @PathVariable Long columnId, @RequestParam("position") int newPosition){
+	@PutMapping("/board/{boardId}/columns/move/{columnId}")
+	public ResponseEntity updateColumn(@PathVariable Long boardId, @PathVariable Long columnId,
+		@RequestParam("position") int newPosition) {
 		return new ResponseEntity(columnService.moveColumn(boardId, columnId, newPosition), HttpStatus.OK);
 	}
 }
