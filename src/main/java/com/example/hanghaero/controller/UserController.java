b01eb973da5cf23e124766f609aaf06535c99cdf
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-26 19:23:18 +0900

    UserController url 수정

--- src/main/java/com/example/hanghaero/controller/UserController.java
+++ src/main/java/com/example/hanghaero/controller/UserController.java
@@ -3,13 +3,16 @@ package com.example.hanghaero.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.hanghaero.dto.user.SignUpRequestDto;
 import com.example.hanghaero.dto.user.UserUpdateResponseDto;
@@ -18,20 +21,37 @@ import com.example.hanghaero.service.UserService;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
-@RestController
+@Slf4j(topic = "UserController")
+@Controller
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/users")
 public class UserController {
 	private final UserService userService;
 
-	@PostMapping("/user/signup")
+	@GetMapping("/signin")
+	public String signInPage(
+		@RequestParam(value = "error", required = false) String error,
+		@RequestParam(value = "exception", required = false) String exception, Model model
+	) {
+		model.addAttribute("error", error);
+		model.addAttribute("exception", exception);
+		return "signin";
+	}
+
+	@GetMapping("/signup")
+	public String signUpPage() {
+		return "signup";
+	}
+
+	@PostMapping("/signup")
 	public ResponseEntity signup(@Valid @RequestBody SignUpRequestDto signupRequestDto) {
 		userService.signup(signupRequestDto);
 		return ResponseEntity.status(HttpStatus.CREATED).body("회원가입에 성공하였습니다.");
 	}
 
-	@PutMapping("/users/{id}")
+	@PutMapping("/{id}")
 	public ResponseEntity<UserUpdateResponseDto> updateUser(@RequestBody SignUpRequestDto signupRequestDto,
 		@PathVariable Long id,
 		@AuthenticationPrincipal UserDetailsImpl userDetails) {
@@ -40,7 +60,7 @@ public class UserController {
 		return new ResponseEntity<>(userService.updateUser(id, signupRequestDto, userId), HttpStatus.OK);
 	}
 
-	@DeleteMapping("/users/{id}")
+	@DeleteMapping("/{id}")
 	public ResponseEntity deleteUser(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
 		Long userId = userDetails.getUser().getId();
 		userService.deleteUser(id, userId);
