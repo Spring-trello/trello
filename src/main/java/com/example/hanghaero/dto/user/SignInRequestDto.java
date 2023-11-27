d3cef32d06532816505b3c788ffe8d423fc9ce4a
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-27 22:06:41 +0900

    테스트를 위해 DTo에 Builder, Constructor

--- src/main/java/com/example/hanghaero/dto/user/SignInRequestDto.java
+++ src/main/java/com/example/hanghaero/dto/user/SignInRequestDto.java
@@ -3,9 +3,15 @@ package com.example.hanghaero.dto.user;
 import com.example.hanghaero.annotation.Password;
 
 import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SignInRequestDto {
 	@Email
 	private String email;
--- src/main/java/com/example/hanghaero/dto/user/SignUpRequestDto.java
+++ src/main/java/com/example/hanghaero/dto/user/SignUpRequestDto.java
@@ -3,9 +3,11 @@ package com.example.hanghaero.dto.user;
 import com.example.hanghaero.annotation.Password;
 
 import jakarta.validation.constraints.Email;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class SignUpRequestDto {
 
 	@Email
