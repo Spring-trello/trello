aeb86043f0a26223f808d8aa75f3a17457fa3558
jungmin.park <pjm9673@gmail.com>
2023-11-28 17:13:21 +0900

    controller html 수정2

--- src/main/java/com/example/hanghaero/controller/ColRestController.java
+++ src/main/java/com/example/hanghaero/controller/ColRestController.java
@@ -18,13 +18,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ColRestController {
 	private final ColService columnService;
-	private final CardService cardService;
+
 	@GetMapping("/boards/{boardId}")
 	public String getColumns(@PathVariable Long boardId, Model model){
 		System.out.println("[ColRestController]" + boardId);
 		System.out.println("[List<ColResponseDto> getColumns ajax 호출]" + boardId);
 		System.out.println(columnService.getColumns(boardId).get(1).getTitle());
 		model.addAttribute("columns", columnService.getColumns(boardId));
-		return "boardtest";
+		return "board";
 	}
 }
