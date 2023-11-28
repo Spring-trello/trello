abb1ef22c29a6f3c30a80ef00dcec766abfdf416
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-28 14:04:41 +0900

    컬럼 생성 및 좌우 스크롤 바 구현

--- src/main/java/com/example/hanghaero/dto/card/CardCreateRequestDto.java
+++ src/main/java/com/example/hanghaero/dto/card/CardCreateRequestDto.java
@@ -1,8 +1,14 @@
 package com.example.hanghaero.dto.card;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CardCreateRequestDto {
 	private Long columnId;
 	private String name;
