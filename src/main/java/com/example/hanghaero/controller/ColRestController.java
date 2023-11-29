13965891ead91b221388e4c004c8f906de148989
jungmin.park <pjm9673@gmail.com>
2023-11-29 11:22:24 +0900

    object not fount exception 추가

--- src/main/java/com/example/hanghaero/controller/ColRestController.java
+++ src/main/java/com/example/hanghaero/controller/ColRestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.hanghaero.dto.column.ColResponseDto;
+import com.example.hanghaero.exception.entity.comment.ObjectNotFoundException;
 import com.example.hanghaero.service.CardService;
 import com.example.hanghaero.service.ColService;
 
@@ -23,7 +24,11 @@ public class ColRestController {
 	public String getColumns(@PathVariable Long boardId, Model model){
 		System.out.println("[ColRestController]" + boardId);
 		System.out.println("[List<ColResponseDto> getColumns ajax 호출]" + boardId);
-		System.out.println(columnService.getColumns(boardId).get(1).getTitle());
+
+		if(columnService.getColumns(boardId) == null){
+			System.out.println("exception 실행");
+			new ObjectNotFoundException();
+		}
 		model.addAttribute("columns", columnService.getColumns(boardId));
 		return "board";
 	}
