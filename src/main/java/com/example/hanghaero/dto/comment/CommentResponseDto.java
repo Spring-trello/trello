0e1bddea3ce3d9e850f0c6f1404bec87ac787536
Gary9409 <gary9409@gmail.com>
2023-11-30 11:59:49 +0900

    카드에 댓글다는 기능 구현

--- src/main/java/com/example/hanghaero/dto/comment/CommentResponseDto.java
+++ src/main/java/com/example/hanghaero/dto/comment/CommentResponseDto.java
@@ -3,6 +3,7 @@ package com.example.hanghaero.dto.comment;
 import java.time.format.DateTimeFormatter;
 
 import com.example.hanghaero.entity.Comment;
+import com.example.hanghaero.entity.User;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,10 +15,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CommentResponseDto {
+	private Long id;
+	private String email;
 	private String contents;
 	private String modifiedAt;
 
 	public CommentResponseDto(Comment comment) {
+		this.id = comment.getId();
+		this.email = comment.getUser().getEmail();
 		this.contents = comment.getComment();
 		this.modifiedAt = comment.getModifiedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 	}
--- src/main/java/com/example/hanghaero/entity/Comment.java
+++ src/main/java/com/example/hanghaero/entity/Comment.java
@@ -4,12 +4,14 @@ import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.example.hanghaero.dto.comment.CommentCreateRequestDto;
 import com.example.hanghaero.dto.comment.CommentModifyRequestDto;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -24,6 +26,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Table(name = "comment")
 @NoArgsConstructor
